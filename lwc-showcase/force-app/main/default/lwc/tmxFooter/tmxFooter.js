2a5364d7fa9bd9ed022b966df483a5877a51d12e
JinGao <jgao@azurgroup.ca>
2021-07-15 14:58:06 -0400

    update logos,todo words and links

--- lwc-showcase/force-app/main/default/lwc/tmxFooter/tmxFooter.js
+++ lwc-showcase/force-app/main/default/lwc/tmxFooter/tmxFooter.js
@@ -1,14 +1,10 @@
 import { LightningElement } from 'lwc';
-import TMX_LOGO from "@salesforce/resourceUrl/jgaoLogo";
+import JG_LOGO from "@salesforce/resourceUrl/jgaoLogo";
 import LINKEDIN_SVG from "@salesforce/resourceUrl/linkedInSqure";
-import FB_SVG from "@salesforce/resourceUrl/facebookSVG";
-import TW_SVG from "@salesforce/resourceUrl/twitterSVG";
-import YT_SVG from "@salesforce/resourceUrl/youtubeSVG";
+import GITHUB_SVG from "@salesforce/resourceUrl/githubSVG";
 
 export default class TmxFooter extends LightningElement {
-    tmxLogoEN = TMX_LOGO;
+    jgaoLogo = JG_LOGO;
     linkedInSVG = LINKEDIN_SVG;
-    facebookSVG = FB_SVG;
-    twitterSVG = TW_SVG;
-    youtubeSVG = YT_SVG;
+    githubSVG = GITHUB_SVG;
 }
