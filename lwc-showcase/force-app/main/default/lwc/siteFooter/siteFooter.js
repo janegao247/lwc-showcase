43d9f11831ab4b5f80d1dbd09dd601468aeeb4e1
JinGao <jgao@azurgroup.ca>
2021-09-27 16:17:23 -0400

    update

--- lwc-showcase/force-app/main/default/lwc/siteFooter/siteFooter.js
+++ lwc-showcase/force-app/main/default/lwc/siteFooter/siteFooter.js
@@ -3,7 +3,7 @@ import JG_LOGO from "@salesforce/resourceUrl/jgaoLogo";
 import LINKEDIN_SVG from "@salesforce/resourceUrl/linkedInSqure";
 import GITHUB_SVG from "@salesforce/resourceUrl/githubSVG";
 
-export default class TmxFooter extends LightningElement {
+export default class SiteFooter extends LightningElement {
     jgaoLogo = JG_LOGO;
     linkedInSVG = LINKEDIN_SVG;
     githubSVG = GITHUB_SVG;
